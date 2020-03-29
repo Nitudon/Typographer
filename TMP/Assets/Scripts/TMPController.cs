f0988b0e5ec4b25350d4d9b357f30afca286ea88
udon <myudongen@gmail.com>
2020-03-29 19:29:48 +0900

    initial commit

---
+++ TMP/Assets/Scripts/TMPController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace TypoGrapher
+{
+    public class TMPController : MonoBehaviour
+    {
+        [SerializeField]
+        private TMP_Text
+    }
+}
