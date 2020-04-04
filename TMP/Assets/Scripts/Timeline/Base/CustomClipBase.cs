049d1c2ac0cce60c3ad6a740eb7e99417737ad39
udon <myudongen@gmail.com>
2020-04-04 17:27:42 +0900

    timeline : create custom clip base

---
+++ TMP/Assets/Scripts/Timeline/Base/CustomClipBase.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+namespace TypoGrapher
+{
+    /// <summary>
+    /// ClipやBehaviourを特定したカスタムClip的なもののベース
+    /// </summary>
+    public abstract class CustomClipBase<T> : PlayableAsset, ITimelineClipAsset
+        where T : PlayableBehaviour, new()
+    {
+        private T _behaviour;
+        public T Behaviour 
+        {
+            get
+            {
+                if (_behaviour == null)
+                {
+                    _behaviour = new T();
+                }
+
+                return _behaviour;
+            }
+        }
+        
+        public abstract ClipCaps clipCaps { get; }
+   
+        public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+        {
+            // BehaviourのPlayableを作って返すだけ
+            return ScriptPlayable<T>.Create(graph);
+        }
+    }
+}
